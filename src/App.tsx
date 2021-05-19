4cde4a33de8d3c01d98ef77e6da299dd17552ba2
Patrick Ullrich <patrick.ullrich93@gmail.com>
2021-05-19 14:05:56 -0600

    latest

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,18 @@ import axios from "axios";
 // const result = await axios("https://swapi.dev/api/people");
 // console.log(result.data.results);
 
+// "name": "Luke Skywalker",
+// "height": "172",
+// "mass": "77",
+// homeworld": "https://swapi.dev/api/planets/1/", -> ability to click on it
+
+// Planet 
+// "name": "Tatooine", 
+// "climate": "arid", 
+// "gravity": "1 standard", 
+// "terrain": "desert", 
+// "population": "200000", 
+
 function App() {
 
   return (
