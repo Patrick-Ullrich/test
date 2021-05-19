90d2f266af0d17e3c84bb32aeeb433fbab377906
Patrick Ullrich <patrick.ullrich93@gmail.com>
2021-05-19 09:59:15 -0600

    some info

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+// const result = await axios("https://swapi.dev/api/people");
+// console.log(result.data.results);
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     test
     </div>
   );
 }
